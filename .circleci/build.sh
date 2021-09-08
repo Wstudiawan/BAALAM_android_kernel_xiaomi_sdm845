7e8d0ca188880d6b13521181d7964dd3e9877c7e
Wstudiawan <manusiasampah7@gmail.com>
2021-09-08 17:18:33 +0530

    update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,45 +1,47 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone -j8 https://github.com/z4nyx/linaro-gcc-elf -b latest-7 --depth=1 gcc
-git clone -j8 --depth=1 https://github.com/Wstudiawan/AnyKernel3-1.git AnyKernel
+git clone --depth=1 https://github.com/Wstudiawan/xRageTC-clang clang
+git clone https://github.com/sohamxda7/llvm-stable -b gcc64 --depth=1 gcc
+git clone https://github.com/sohamxda7/llvm-stable -b gcc32  --depth=1 gcc32
+git clone --depth=1 https://github.com/Wstudiawan/AnyKernel3-1.git AnyKernel
 echo "Done"
-GCC="$(pwd)/gcc/bin/aarch64-elf-"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
-TANGGAL=$(date +"%S-%F")
+TANGGAL=$(date +"%F-%S")
 START=$(date +"%s")
+KERNEL_DIR=$(pwd)
+PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
+export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
 export ARCH=arm64
-export KBUILD_BUILD_USER=wstudiawan
-export KBUILD_BUILD_HOST=circle
-git config --global user.email "manusiasampah7@gmail.com"
-git config --global user.name "Wstudiawan"
+export KBUILD_BUILD_HOST=circleci
+export KBUILD_BUILD_USER="wstudiawan"
 # sticker plox
 function sticker() {
     curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
-        -d sticker="CAADAQADHAADvi-SJce2yqOIKPDcFgQ" \
+        -d sticker="CAACAgEAAxkBAAEnKnJfZOFzBnwC3cPwiirjZdgTMBMLRAACugEAAkVfBy-aN927wS5blhsE" \
         -d chat_id=$chat_id
 }
 # Send info plox channel
 function sendinfo() {
     curl -s -X POST "https://api.telegram.org/bot1446507242:AAFivf422Yvh3CL7y98TJmxV1KgyKByuPzM/sendMessage" \
-        -d chat_id="$chat_id" \
+        -d chat_id="-1001421078455" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
-        -d text="<b>-Parak Karakah Kernel- CI</b>%0AStarted on: <code>Circle CI/CD</code>%0ADevice: <b>Xiaomi Pocophone F1</b> (beryllium)%0ABranch: <code>$(git rev-parse --abbrev-ref HEAD)</code>%0AUnder Commit: <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AToolchain: <code>aarch64-elf-gcc (Linaro GCC) 7.5.0</code>%0ADate: <code>$(date)</code>%0A<b>Build Status:</b> #Nightly"
+        -d text="<b>• Parak Karakah Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Xiaomi Pocophone F1</b> (beryllium)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>${KBUILD_COMPILER_STRING}</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#Stable"
 }
 # Push kernel to channel
 function push() {
-    cd AnyKernel || exit 1
+    cd AnyKernel
     ZIP=$(echo *.zip)
     curl -F document=@$ZIP "https://api.telegram.org/bot1446507242:AAFivf422Yvh3CL7y98TJmxV1KgyKByuPzM/sendDocument" \
-        -F chat_id="$chat_id" \
+        -F chat_id="-1001421078455" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
-        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Pocophone F1 (beryllium)</b>"
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Pocophone F1(beryllium)</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
 }
 # Fin Error
 function finerr() {
     curl -s -X POST "https://api.telegram.org/bot1446507242:AAFivf422Yvh3CL7y98TJmxV1KgyKByuPzM/sendMessage" \
-        -d chat_id="$chat_id" \
+        -d chat_id="-1001421078455" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=markdown" \
         -d text="Build throw an error(s)"
@@ -47,8 +49,14 @@ function finerr() {
 }
 # Compile plox
 function compile() {
-    make -s -C "$(pwd)" -j"$(nproc)" O=out beryllium_defconfig
-    make -C "$(pwd)" CROSS_COMPILE="${GCC}" O=out -j"$(nproc)"
+    make O=out ARCH=arm64 beryllium_defconfig
+    make -j$(nproc --all) O=out \
+                    ARCH=arm64 \
+                    CC=clang \
+                    CLANG_TRIPLE=aarch64-linux-gnu- \
+                    CROSS_COMPILE=aarch64-linux-android- \
+                    CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
     if ! [ -a "$IMAGE" ]; then
         finerr
         exit 1
@@ -58,8 +66,8 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Parak-Karakah-Kernel-beryllium-${TANGGAL}.zip *
-    cd .. #well
+    zip -r9 ParakKarakah-${TANGGAL}.zip *
+    cd ..
 }
 sticker
 sendinfo
@@ -68,3 +76,20 @@ zipping
 END=$(date +"%s")
 DIFF=$(($END - $START))
 push
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
