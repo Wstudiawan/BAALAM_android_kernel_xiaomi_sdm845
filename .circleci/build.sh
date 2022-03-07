709721462dc8eb57146dc4e7d3dd8d77fd148833
Wahyu Studiawan <manusiasampah7@gmail.com>
2022-03-07 22:44:10 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -44,7 +44,7 @@ FINAL_ZIP=${ZIPNAME}-${VERSION}-${DEVICE}-${DRONE_BUILD_NUMBER}.zip
 
 ##----------------------------------------------------------##
 # Specify compiler ( azure , eva gcc , aosp , neutron & proton )
-COMPILER=gcc64
+COMPILER=eva
 
 ##----------------------------------------------------------##
 # Specify Linker
